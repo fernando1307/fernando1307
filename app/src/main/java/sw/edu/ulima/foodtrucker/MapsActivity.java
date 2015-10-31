3f752fe16e67a35edc5ca2c2f636f502c75340dc
fernando1307 <fernando130794@gmail.com>
2015-10-31 10:58:25 -0500

    v6.0  ya funciona el mapa

--- app/src/main/java/sw/edu/ulima/foodtrucker/MapsActivity.java
+++ app/src/main/java/sw/edu/ulima/foodtrucker/MapsActivity.java
@@ -3,67 +3,61 @@ package sw.edu.ulima.foodtrucker;
 import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    final ParseUser currentUser = ParseUser.getCurrentUser();
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        System.out.println("si entra on create");
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-        setUpMapIfNeeded();
+
+
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(total, 11));
+        mMap.setMyLocationEnabled(true);
+        mMap.setTrafficEnabled(true);
+
+        UiSettings u=mMap.getUiSettings();
+        u.setZoomControlsEnabled(true);
+
+
+
+    }
+    @Override
+    public void onBackPressed(){
+        MapsActivity.this.finish();
     }
 
-    private void setUpMapIfNeeded() {
-        //null check para confirmar que el mapa no se ha inicializado
-        if (mMap == null) {
-            //obtener el mapa del SupportMapFragment
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            System.out.println("si entra setupmapifneeded");
-            // ver si hubo exito
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-    private void setUpMap() {
-        System.out.println("si entra setupmap");
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
 
 
-            // Get longitude of the current location
+}
 
-            //String userId = "O6pGXcJy3C";
-            //String username = "Nick";
-            //ParseObject globeobject = new ParseObject("global");
-            //globeobject.put("username",currentUser.getString("name"));
-
-            //globeobject.put("userID",userId);
-
-
-
-
-            //final ParseGeoPoint point = new ParseGeoPoint(latitude, longitude);
-            //globeobject.put("Location", point);
-
-        }
-    }
 
